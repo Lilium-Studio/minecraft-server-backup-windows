98e59aaa9f4f28df5e1b430685406c48f11e8eb1
Lawaxi <2330234142@qq.com>
2020-06-24 12:18:56 +0800

    添加项目文件。

---
+++ minecraft-server-backup-windows/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace minecraft_server_backup_windows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            // 
+            // textBox3
+            // 
+            resources.ApplyResources(this.textBox3, "textBox3");
+            this.textBox3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            // 
+            // progressBar1
+            // 
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.Name = "progressBar1";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            resources.ApplyResources(this.linkLabel1, "linkLabel1");
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // checkBox1
+            // 
+            resources.ApplyResources(this.checkBox1, "checkBox1");
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ minecraft-server-backup-windows/Form1.cs
@@ -0,0 +1,178 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.IO;
+using System.Runtime.Remoting.Messaging;
+using System.Windows.Forms;
+
+namespace minecraft_server_backup_windows
+{
+    public partial class Form1 : Form
+    {
+        static String cfg;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 获取运行目录
+            String td = System.IO.Directory.GetCurrentDirectory();
+            if (td.EndsWith("\\"))
+                td.Remove(td.Length - 1);
+
+            // 配置文件目录
+            cfg = td + "\\config.json";
+
+            textBox1.Text = getCfg();
+            if (textBox1.Text.Equals(""))
+            {
+                textBox1.Text = td + "\\world\\";
+            }
+
+            textBox2.Text = td + "\\world_"
+                + DateTime.Today.Year + "-" + DateTime.Today.Month + "-" + DateTime.Today.Day + "-"
+                + DateTime.Now.Hour + "-" + DateTime.Now.Minute + ".zip";
+        }
+
+        String getCfg()
+        {
+            if (!File.Exists(cfg))
+                return "";
+
+            try
+            {
+                StreamReader file = File.OpenText(cfg);
+                JsonTextReader reader = new JsonTextReader(file);
+                JObject jsonObject = (JObject)JToken.ReadFrom(reader);
+
+                if (!jsonObject.ContainsKey("world-folder"))
+                {
+                    return "";
+                }
+
+                String a = (String)jsonObject["world-folder"];
+
+                reader.Close();
+                file.Close();
+
+                return a;
+
+            }
+            catch (Exception e)
+            {
+                return "";
+            }
+
+        }
+
+        void saveCfg()
+        {
+            JObject jsonObject = new JObject();
+            jsonObject["world-folder"] = textBox1.Text;
+            File.WriteAllText(cfg, Newtonsoft.Json.JsonConvert.SerializeObject(jsonObject, Newtonsoft.Json.Formatting.Indented));
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            saveCfg();
+try
+            {
+                packing(textBox1.Text, textBox2.Text, checkBox1.Checked);
+            }
+            catch (Exception e1)
+            {
+                if (e1 is UnauthorizedAccessException)
+                {
+                    print("[ERROR] 目录拒绝储存.");
+
+                }
+                else
+                {
+                    print("[ERROR] 未知错误导致压缩失败.");
+
+                }
+
+                progressBar1.Value = progressBar1.Maximum;
+            }
+        }
+
+        static ArrayList files;
+
+        void print(String text)
+        {
+            textBox3.Text += text + "\r\n";
+        }
+
+        void packing(string folderName, string zipName, bool over)
+        {
+            progressBar1.Value = 0;
+
+            if (folderName.EndsWith(@"\"))
+                folderName = folderName.Remove(folderName.Length - 1);
+
+            if (!System.IO.Directory.Exists(folderName))
+            {
+                print("[ERROR] " + folderName + " 不存在!");
+                progressBar1.Value = progressBar1.Maximum;
+                return;
+            }
+
+            if (!over && System.IO.File.Exists(zipName))
+            {
+                print("[ERROR] " + components + " 已经存在,您没有勾选覆盖!");
+                progressBar1.Value = progressBar1.Maximum;
+                return;
+            }
+
+            files = new ArrayList();
+            DirectoryInfo di = new DirectoryInfo(folderName);
+            int size = 0;
+            foreach (DirectoryInfo item in di.GetDirectories())
+            {
+                files.Add(item.FullName);
+                print("[SCANER] " + item.FullName);
+                size++;
+            }
+
+            progressBar1.Maximum = size;
+
+            FileStream b = File.Create(zipName);
+            ZipOutputStream zipOutputStream = new ZipOutputStream(b);
+
+            foreach (String file in files)
+            {
+                print("[PACKING] " + file);
+
+                FileStream fs = File.OpenRead(file);
+                byte[] buffer = new byte[fs.Length];
+                fs.Read(buffer, 0, buffer.Length);
+                zipOutputStream.PutNextEntry(new ZipEntry(file));
+                zipOutputStream.Write(buffer, 0, buffer.Length);
+                fs.Close();
+            }
+
+            zipOutputStream.Close();
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("你事有多懒，广告都不能去浏览器自己输一下。");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String td = System.IO.Directory.GetCurrentDirectory();
+            if (td.EndsWith("\\"))
+                td.Remove(td.Length - 1);
+
+            textBox1.Text = td + "\\world\\";
+        }
+    }
+}
